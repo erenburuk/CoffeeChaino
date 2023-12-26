a1332dd3458f49040b80a0cc5431a874521100da
Emir0zcelik <themirozcelik@gmail.com>
2023-12-26 15:39:23 +0300

    Smart Contrat Fixed

--- client/src/pages/AddProduct.jsx
+++ client/src/pages/AddProduct.jsx
@@ -1,4 +1,143 @@
+
+import Web3 from 'web3';
+const web3 = new Web3(Web3.givenProvider);
+
 const AddProduct = () => {
+async function interactWithContract() {
+
+	const address = "0x56f4C35a505cB752b1BC6c0EcfE4443c89Abfc96";
+	const abi = [
+		{
+			"inputs": [
+				{
+					"internalType": "uint256",
+					"name": "_productID",
+					"type": "uint256"
+				},
+				{
+					"internalType": "uint256",
+					"name": "_productWeight",
+					"type": "uint256"
+				}
+			],
+			"name": "buyProduct",
+			"outputs": [],
+			"stateMutability": "payable",
+			"type": "function"
+		},
+		{
+			"inputs": [
+				{
+					"internalType": "uint256",
+					"name": "_productID",
+					"type": "uint256"
+				}
+			],
+			"name": "delivery",
+			"outputs": [],
+			"stateMutability": "nonpayable",
+			"type": "function"
+		},
+		{
+			"inputs": [
+				{
+					"internalType": "string",
+					"name": "_title",
+					"type": "string"
+				},
+				{
+					"internalType": "string",
+					"name": "_description",
+					"type": "string"
+				},
+				{
+					"internalType": "uint256",
+					"name": "_price",
+					"type": "uint256"
+				},
+				{
+					"internalType": "uint256",
+					"name": "_weight",
+					"type": "uint256"
+				}
+			],
+			"name": "registerProduct",
+			"outputs": [],
+			"stateMutability": "nonpayable",
+			"type": "function"
+		},
+		{
+			"inputs": [],
+			"name": "getNum",
+			"outputs": [
+				{
+					"internalType": "int256",
+					"name": "",
+					"type": "int256"
+				}
+			],
+			"stateMutability": "view",
+			"type": "function"
+		},
+		{
+			"inputs": [
+				{
+					"internalType": "uint256",
+					"name": "",
+					"type": "uint256"
+				}
+			],
+			"name": "products",
+			"outputs": [
+				{
+					"internalType": "uint256",
+					"name": "productID",
+					"type": "uint256"
+				},
+				{
+					"internalType": "string",
+					"name": "title",
+					"type": "string"
+				},
+				{
+					"internalType": "uint256",
+					"name": "price",
+					"type": "uint256"
+				},
+				{
+					"internalType": "uint256",
+					"name": "weight",
+					"type": "uint256"
+				},
+				{
+					"internalType": "string",
+					"name": "description",
+					"type": "string"
+				},
+				{
+					"internalType": "address payable",
+					"name": "seller",
+					"type": "address"
+				},
+				{
+					"internalType": "address",
+					"name": "buyer",
+					"type": "address"
+				},
+				{
+					"internalType": "bool",
+					"name": "delivered",
+					"type": "bool"
+				}
+			],
+			"stateMutability": "view",
+			"type": "function"
+		}
+	];
+	const contract = new web3.eth.Contract(abi, address);
+
+	const bal = await contract.methods.getNum().call();
+}
   return (
     <section class='py-12 sm:py-16'>
       <div class='container mx-auto px-4'>
@@ -167,6 +306,14 @@ const AddProduct = () => {
           </div>
         </div>
       </div>
+      <a
+        href='#'
+        onClick={interactWithContract}
+        class='mt-2 inline-flex items-center justify-center rounded-xl border border-transparent bg-blue-600 px-5 py-3 font-medium text-white hover:bg-blue-700'
+      >
+        {' '}
+        Get started{' '}
+      </a>
     </section>
   );
 };
