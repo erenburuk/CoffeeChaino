01c2cbe691e199fc0bdd7280efd0b0bc8567ca80
Emir0zcelik <themirozcelik@gmail.com>
2023-12-28 21:12:19 +0300

    comments deleted

--- server/server.js
+++ server/server.js
@@ -25,22 +25,12 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-
-
 app.post('/upload-image', upload.single('file'), async (req, res) => {
   try {
-    console.log(req.file);
-    console.log(req.file.filename);
-    // Gelen resim verisi
-    const imageData = req.body.image; // Bu kısım gelen resim verisinin hangi şekilde gönderileceğine bağlı olarak değişebilir
-    console.log("image:" + imageData);
-    // Örnek olarak dosya oluşturup içeriğini almak
-    // Bu kısmı kendi ihtiyaçlarınıza göre ayarlayın, belki form-data veya başka bir yöntem kullanmanız gerekebilir
-    //fs.writeFileSync('tempImage.jpg', './uploads/' + req.file.filename, 'base64');
+    const imageData = req.body.image; 
 
-    // Pinata'ya yükleme yapacak istek
-    const pinataApiKey = 'e3b3c4da41b644547eb7'; // Pinata API anahtarınızı buraya ekleyin
-    const pinataSecretApiKey = '44d9ac5ac32f0040793cf2934965b00d9ef471b270ef57a9ca08db83b8ce2e08'; // Pinata gizli API anahtarınızı buraya ekleyin
+    const pinataApiKey = 'e3b3c4da41b644547eb7'; 
+    const pinataSecretApiKey = '44d9ac5ac32f0040793cf2934965b00d9ef471b270ef57a9ca08db83b8ce2e08'; 
 
     const data = new FormData();
     data.append('file', fs.createReadStream('./uploads/' + req.file.filename));
@@ -54,11 +44,7 @@ app.post('/upload-image', upload.single('file'), async (req, res) => {
       },
     });
 
-    // Pinata'dan gelen cevap
     console.log(pinataResponse.data);
-
-    // Temizlik: Geçici dosyayı sil
-    //fs.unlinkSync('tempImage.jpg');
 
     res.status(200).json({ success: true, pinataResponse: pinataResponse.data });
   } catch (error) {
@@ -66,8 +52,6 @@ app.post('/upload-image', upload.single('file'), async (req, res) => {
     res.status(500).json({ success: false, error: error.message });
   }
 });
-
-
 
 app.listen(5000, () => {
   console.log('Server is running on port 5000');
